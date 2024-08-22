5c95200f6913c9db09f5ee0edc04356c8f44b2b5
SHAKIR <shakirsjd@gmail.com>
2024-08-22 21:13:17 +0530

    initialized the chat-app project

---
+++ backend/controllers/auth.controler.js
@@ -0,0 +1,14 @@
+export const login = (req,res)=>{
+    console.log("this ")
+    res.send("this is login route")
+}
+
+export const logout = (req,res)=>{
+    console.log("this ")
+    res.send("this is logout route")
+}
+
+export const signup = (req,res)=>{
+    console.log("this ")
+    res.send("this is signup route")
+}
