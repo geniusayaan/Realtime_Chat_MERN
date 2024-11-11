ece51b9c8ba24cdab119369e0a88180e2fd38148
SHAKIR <shakirsjd@gmail.com>
2024-11-11 18:41:47 +0530

    ADDED ENV TO FRONTEND

--- frontend/src/context/Socketcontext.jsx
+++ frontend/src/context/Socketcontext.jsx
@@ -15,7 +15,7 @@ export const SocketcontextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000",{
+      const socket = io(import.meta.env.VITE_BACKEND_URL,{
         query:{
           userId:authUser._id
         }
